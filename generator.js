a7f2834f65f1793088ab2aee409f575212df16ec
Gage <gagemw@gmail.com>
2020-04-05 03:17:56 -0600

    Code data generator

---
+++ generator.js
@@ -0,0 +1,61 @@
+const faker = require("faker");
+const fs = require("fs");
+const { PerformanceObserver, performance } = require('perf_hooks');
+
+const obs = new PerformanceObserver((items) => {
+  console.log(Math.round(items.getEntries()[0].duration/60000)+'m');
+  performance.clearMarks();
+});
+obs.observe({ entryTypes: ['measure'] });
+
+
+
+var qid = 3521634;
+var aid = 12392946;
+var phid = 3717892;
+
+const question = () => {
+  let output = {
+    id: ++qid,
+    product_id: Math.ceil(Math.random() * 1000011),
+    body: faker.lorem.sentence().slice(0, -1) + "?",
+    date_written: faker.date.past(5).toJSON().slice(0, 10),
+    asker_name: faker.name.firstName(),
+    asker_email: faker.internet.email(),
+    reported: Math.random() > 0.8 ? 1 : 0,
+    helpful: Math.floor(Math.random() * 25),
+  };
+  return `${output.id},${output.product_id},"${output.body}","${output.date_written}","${output.asker_name}","${output.asker_email}",${output.reported},${output.helpful}\n`;
+};
+
+const answer = () => {
+  let output = {
+    id: ++aid,
+    question_id: Math.ceil(Math.random() * 10000000) + 3521634,
+    body: faker.lorem.sentence(),
+    date_written: faker.date.past(5).toJSON().slice(0, 10),
+    answerer_name: faker.name.firstName(),
+    answerer_email: faker.internet.email(),
+    reported: Math.random() > 0.8 ? 1 : 0,
+    helpful: Math.floor(Math.random() * 25),
+  };
+  return `${output.id},${output.question_id},"${output.body}","${output.date_written}","${output.answerer_name}","${output.answerer_email}",${output.reported},${output.helpful}\n`;
+};
+
+const photo = () => {
+  let output = {
+    id: ++phid,
+    answer_id: Math.ceil(Math.random() * 10000000) + 12392946,
+    url: faker.image.fashion(),
+  };
+  return `${output.id},${output.answer_id},"${output.url}"\n`;
+};
+
+performance.mark("A");
+for (let i = 0; i < 20; i++) {
+  fs.appendFileSync("./newquestions.csv", question());
+  fs.appendFileSync("./newanswers.csv", answer());
+  fs.appendFileSync("./newphotos.csv", photo());
+}
+performance.mark("B");
+performance.measure("A to B", "A", "B");
