0ae51992eaa6c2679c317cd9884527b8292cfcf1
Gage <gagemw@gmail.com>
2020-04-07 20:14:37 -0600

    Work on server routes

--- app.js
+++ app.js
@@ -13,13 +13,14 @@ app.use(bodyParser.json());
 // app.use(express.static(path.join(__dirname, "../client/public")));
 
 app.get("/qa/:product_id/", (req, res) => {
+  let count = Number(req.query.count) || 5;
   db.query(
     aql`for q in questions
-  filter q.product_id == ${Number(req.params.product_id)}
-  limit ${Number(req.query.count) || 5}
+  filter q.product_id == ${Number(req.params.product_id)} && q.reported == 0
+  limit ${count}
   return merge(q,{answers:merge(
     for a in answers
-    filter to_number(q._key) == a.question_id
+    filter to_number(q._key) == a.question_id && a.reported == 0
     return {[a._key]:merge(a,{photos:
       (for p in answers_photos
       filter to_number(a._key) == p.answer_id
@@ -53,17 +54,25 @@ app.get("/qa/:product_id/", (req, res) => {
         }
         return temp;
       });
-      res.send({ product_id: req.params.product_id, results: output });
+      res.send({
+        product_id: req.params.product_id,
+        count: count,
+        results: output,
+      });
     })
-    .catch((error) => console.error(error));
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(400);
+    });
 });
 
 app.get("/qa/:question_id/answers/", (req, res) => {
+  let count = Number(req.query.count) || 5;
   db.query(
     aql`
     for a in answers
-    filter ${Number(req.params.question_id)} == a.question_id
-    limit ${Number(req.query.count) || 5}
+    filter ${Number(req.params.question_id)} == a.question_id && a.reported == 0
+    limit ${count}
     return merge(a,{photos:
       (for p in answers_photos
       filter to_number(a._key) == p.answer_id
@@ -72,23 +81,86 @@ app.get("/qa/:question_id/answers/", (req, res) => {
     })`
   )
     .then((result) => {
-      
-      res.send({ question: req.params.question_id, results: result._result });
+      let output = result._result.map((answer) => {
+        return {
+          answer_id: Number(answer._key),
+          body: answer.body,
+          date: answer.date_written,
+          answerer_name: answer.answerer_name,
+          helpfulness: answer.helpful,
+          photos: answer.photos.map((photo) => {
+            return {
+              id: Number(photo._key),
+              url: photo.url,
+            };
+          }),
+        };
+      });
+      res.send({
+        question: req.params.question_id,
+        count: count,
+        results: output,
+      });
     })
-    .catch((error) => console.error(error));
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(400);
+    });
 });
 
-app.post("/qa/:product_id");
+app.post("/qa/:product_id", (req, res) => {
+  db.query(
+    aql`
+  insert {
+    product_id:${Number(req.params.product_id)}, 
+    body:${req.body.body},
+    date_written: ${new Date().toISOString().slice(0, -14)},
+    asker_name:${req.body.name}, 
+    asker_email:${req.body.email}, 
+    reported: 0, 
+    helpful: 0
+  } into questions
+`
+  )
+    .then(() => res.sendStatus(201))
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(400);
+    });
+});
 
-app.post("/qa/:question_id/answers");
+app.post("/qa/:question_id/answers", (req, res) => {
+  // db.query(
+  //   aql`
+  // insert {
+  //   question_id:${Number(req.params.question_id)}, 
+  //   body:${req.body.body},
+  //   date_written: ${new Date().toISOString().slice(0, -14)},
+  //   answerer_name:${req.body.name}, 
+  //   answerer_email:${req.body.email}, 
+  //   reported: 0, 
+  //   helpful: 0
+  // } into answers
+  // let inserted = NEW
+  // let photos = ${req.body.photos}                                            DOES NOT PROPERLY READ AS ARANGO ARRAYs
+  // for p in photos
+  // insert {answer_id:to_number(inserted._key),url:p} into answers_photos
+  // `
+  // )
+  //   .then(() => res.sendStatus(201))
+  //   .catch((error) => {
+  //     console.error(error);
+  //     res.sendStatus(400);
+  //   });
+});
 
-app.put("/qa/question/:question_id/helpful");
+app.put("/qa/question/:question_id/helpful", (req, res) => {});
 
-app.put("/qa/question/:question_id/report");
+app.put("/qa/question/:question_id/report", (req, res) => {});
 
-app.put("/qa/answer/:answer_id/helpful");
+app.put("/qa/answer/:answer_id/helpful", (req, res) => {});
 
-app.put("/qa/answer/:answer_id/report");
+app.put("/qa/answer/:answer_id/report", (req, res) => {});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
