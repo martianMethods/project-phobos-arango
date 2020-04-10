be68e8b3f17079d599917751df97911de7de9df5
Gage <gagemw@gmail.com>
2020-04-10 15:52:44 -0600

    Prepare for production

--- newrelic.js
+++ newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: 'LICENSE KEY GOES HERE',
+  license_key: require('newrelicAuth.js') || process.env.newrelicAuth || 'ADD NEW RELIC AUTH',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
---
+++ newrelicAuthExample.js
@@ -0,0 +1,1 @@
+module.exports = newrelicAuth = 'INSERT AUTH KEY HERE'
